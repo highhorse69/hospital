59070458b2d7d110f3878e3c6960021ce7a62a41
Rohan Sahani <rohansahani@Rohans-MacBook-Air.local>
2021-09-09 09:50:54 +0530

    done

--- src/pages/Internship/Internship.js
+++ src/pages/Internship/Internship.js
@@ -114,7 +114,7 @@ Registration will be closed on 29th Sept, 2021.
             <h3><b>Internship Fees:</b></h3>
             <pre>Rs. 4500/-</pre>
            <b> <pre>Certificate will be provided,after completion of internship</pre></b>
-          <b> <pre>450+ students intership,grab it soon !!</pre></b>
+          <b> <pre>450+ students intership</pre></b>
              &nbsp;
 
 
